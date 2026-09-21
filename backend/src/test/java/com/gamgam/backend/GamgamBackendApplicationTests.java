010ccbf299f9294243872eb4058e11a9ff33903f
Jung WonSeok <114576793+RDavid32@users.noreply.github.com>
2026-09-21 15:55:35 +0900

    build : backend javaspring 프로젝트 생성

---
+++ backend/src/test/java/com/gamgam/backend/GamgamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamgam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamgamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
