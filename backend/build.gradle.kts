dc136ff11265c6e9ef54b33f9610bcd5eb9d126c
whdwon <whdwon09@gmail.com>
2026-09-26 22:57:03 +0900

    Chore: JPA/H2 의존성 및 환경설정 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
